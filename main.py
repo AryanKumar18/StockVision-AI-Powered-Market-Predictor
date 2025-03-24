44e298fe3a9e53d003045f5fff0ba93d4a908449
Aryan Kumar <150644755+AryanKumar18@users.noreply.github.com>
2025-03-24 21:53:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -171,8 +171,8 @@ st.sidebar.markdown("### Areas of Interest")
 st.sidebar.info("""
 **Aryan Kumar**  
                 
-💻 Tech Stack 
-📍 Future Aspirations  
-💼 Contact: +91 7975527076  
-🔗 Email: aryankumar.30.2003@gmail.com
+• Tech Stack 
+• Future Aspirations  
+• Contact: +91 7975527076  
+• Email: aryankumar.30.2003@gmail.com
 """)
